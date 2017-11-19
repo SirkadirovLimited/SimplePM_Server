a067a707b83e265038ea617785e94ca27bc23b7e
Kadirov Yurij <admin@sirkadirov.com>
2017-11-19 12:02:31 +0200

    Обновление "НОЯБРЬ-19"
    - Удалёны устаревшие и не используемые ветви кода
    - На этом пока что всё!

--- SimplePM_Server/SimplePM_Submission.cs
+++ SimplePM_Server/SimplePM_Submission.cs
@@ -28,60 +28,6 @@ namespace SimplePM_Server
     {
 
         ///////////////////////////////////////////////////
-        /// Язык программирования, на котором
-        /// было написано решение
-        ///////////////////////////////////////////////////
-
-        public enum SubmissionLanguage
-        {
-
-            Unset,
-            Freepascal,
-            CSharp,
-            Cpp,
-            C,
-            Python,
-            Lua,
-            Java,
-            PHP
-            
-        }
-
-        ///////////////////////////////////////////////////
-        /// Функция возвращает enum языка программирования
-        /// по его текстовому названию
-        ///////////////////////////////////////////////////
-
-        public static SubmissionLanguage GetCodeLanguageByName(string codeLang)
-        {
-
-            switch (codeLang)
-            {
-
-                case "freepascal":
-                    return SubmissionLanguage.Freepascal;
-                case "csharp":
-                    return SubmissionLanguage.CSharp;
-                case "cpp":
-                    return SubmissionLanguage.Cpp;
-                case "c":
-                    return SubmissionLanguage.C;
-                case "python":
-                    return SubmissionLanguage.Python;
-                case "lua":
-                    return SubmissionLanguage.Lua;
-                case "java":
-                    return SubmissionLanguage.Java;
-                case "php":
-                    return SubmissionLanguage.PHP;
-                default:
-                    return SubmissionLanguage.Unset;
-
-            }
-
-        }
-
-        ///////////////////////////////////////////////////
         /// Функция возвращает расширение файла
         /// исходного кода программы по имени
         /// языка программирования
