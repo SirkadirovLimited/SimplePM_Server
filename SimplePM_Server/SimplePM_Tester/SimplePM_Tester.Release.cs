e582e0aa4d283bc3f3650f7b4291c887071b4580
Kadirov Yurij <admin@sirkadirov.com>
2018-01-09 22:26:55 +0200

    Обновление "ЯНВАРЬ-09"
    - Продолжается реализация предложений и замечаний, подробно описанных в вопросе #12
    - Качество кода улучшено
    - Улучшена общая удобочитабельность и стилистика исходного кода приложения
    - Багов нет, но Вы держитесь!
    - На этом пока что всё!

---
+++ SimplePM_Server/SimplePM_Tester/SimplePM_Tester.Release.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Copyright (C) 2017, Kadirov Yurij.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ * 
+ * @Author: Kadirov Yurij
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
+ */
+/*! \file */
+
+using System;
+using System.Collections.Generic;
+using CompilerBase;
+using IniParser.Model;
+using MySql.Data.MySqlClient;
+
+namespace SimplePM_Server.SimplePM_Tester
+{
+
+    internal partial class SimplePM_Tester
+    {
+
+        public void Release()
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+    }
+
+}
