3e823aa28b9edca63adb49a2206500b9d76f7174
Kadirov Yurij <admin@sirkadirov.com>
2018-01-26 14:45:47 +0200

    Реализации интерфейсов модулей компиляции обновлены в соответствии со спецификациями

--- ICompilerPlugin.PHP/NoCompiler.cs
+++ ICompilerPlugin.PHP/NoCompiler.cs
@@ -50,7 +50,7 @@ namespace CompilerPlugin
         /// результата компиляции данной программы.
         ///////////////////////////////////////////////////
 
-        public CompilerResult StartCompiler(ref IniData sConfig, string submissionId, string fileLocation)
+        public CompilerResult StartCompiler(ref IniData sConfig, ref IniData sCompilersConfig, string submissionId, string fileLocation)
         {
 
             //Делаем преждевременные выводы
@@ -83,7 +83,7 @@ namespace CompilerPlugin
         /// пользовательской программы.
         ///////////////////////////////////////////////////
 
-        public bool SetRunningMethod(ref IniData sConfig, ref ProcessStartInfo startInfo, string filePath)
+        public bool SetRunningMethod(ref IniData sConfig, ref IniData sCompilersConfig, ref ProcessStartInfo startInfo, string filePath)
         {
             try
             {
