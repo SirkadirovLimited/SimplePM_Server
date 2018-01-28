fd47d1d57325a42eee42df3827bbaf9e4ff105e8
Kadirov Yurij <admin@sirkadirov.com>
2018-01-28 10:33:53 +0200

    Продолжаю реализацию новой архитектуры сервера проверки решений #12

---
+++ PluginBase/IServerPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginBase
+{
+
+    public interface IServerPlugin
+    {
+
+        void IServerPlugin(ref ServerEvents serverEvents);
+
+    }
+
+}
