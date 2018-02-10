682e6004951db3c5c93a21259af3479da762f354
Kadirov Yurij <admin@sirkadirov.com>
2018-02-10 16:06:43 +0200

    Продолжается реализация новой архитектуры сервера проверки решений #12 и другое #13

--- SimplePM_Server/SimplePM_Tester/SimplePM_Tester.Release.cs
+++ SimplePM_Server/SimplePM_Tester/SimplePM_Tester.Release.cs
@@ -8,13 +8,14 @@
  * @Email: admin@sirkadirov.com
  * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
  */
-/*! \file */
 
 using System;
 using System.Collections.Generic;
+using System.Web;
 using CompilerBase;
 using IniParser.Model;
 using MySql.Data.MySqlClient;
+using ReleaseTestInfo;
 
 namespace SimplePM_Server.SimplePM_Tester
 {
@@ -22,10 +23,184 @@ namespace SimplePM_Server.SimplePM_Tester
     internal partial class SimplePM_Tester
     {
 
-        public void Release()
+        private Queue<ReleaseTest> ReleaseTestsInfo;
+
+        public ProgramTestingResult Release()
         {
 
+            /*
+             * Получаем информацию о тестах
+             * для     release-тестирования
+             * пользовательского    решения
+             * посталвенной задачи.
+             */
+            ReleaseTestsInfo = GetTestsInfo();
+
+            /*
+             * Сохраняем первоначальное количество
+             * тестов в отдельной переменной, дабы
+             * не затерять   эту   информацию  при
+             * видоизменении очереди.
+             */
+            var testsCount = ReleaseTestsInfo.Count;
+
+            /*
+             * Создаём объект,  который будет
+             * хранить  результаты  релизного
+             * тестирования пользовательского
+             * решения  поставленной  задачи.
+             */
+            var programTestingResult = new ProgramTestingResult(testsCount);
+
+            /*
+             * В цикле  тестируем  пользовательское
+             * решение   поставленной   задачи   на
+             * заранее  подготовленных   тестах   и
+             * записываем  результаты  проверки  по
+             * каждому тесту в специально созданный
+             * для этих нужд массив.
+             */
+            for (var currentTestIndex = 0; currentTestIndex < testsCount; currentTestIndex++)
+            {
+
+                /*
+                 * Делаем выборку  данных о текущем
+                 * релизном тесте из очереди тестов
+                 */
+                var currentTest = ReleaseTestsInfo.Dequeue();
+
+                /*
+                 * Запускаем  тестирование  пользовательского
+                 * решения поставленной задачи  на  указанном
+                 * тесте и получаем  промежуточные результаты
+                 * тестирования, заносим их в соответственную
+                 * переменную,  созданную специально для этих
+                 * нужд.
+                 */
+                var currentTestResult = new ProgramTester(
+                    ref sConfig,
+                    ref sCompilersConfig,
+                    ref _compilerPlugins,
+                    submissionInfo.CodeLang,
+                    exeFileUrl,
+                    "",
+                    currentTest.MemoryLimit,
+                    currentTest.ProcessorTimeLimit,
+                    currentTest.InputData.ToString(),
+                    currentTest.OutputData.Length * 2,
+                    true
+                ).RunTesting();
+
+                /*
+                 * Заносим результат проведения
+                 * текущего теста в специальный
+                 * массив.
+                 */
+                programTestingResult.TestingResults[currentTestIndex] = currentTestResult;
+
+            }
+
             throw new NotImplementedException();
+
+        }
+
+        private Queue<ReleaseTest> GetTestsInfo()
+        {
+
+            /*
+             * Производим выборку всех данных,
+             * необходимых для формирования
+             * релизных тестов пользовательского
+             * решения поставленной задачи
+             */
+            
+            // Формируем SQL-запрос
+            const string querySelect = @"
+                SELECT 
+                    * 
+                FROM 
+                    `spm_problems_tests`
+                WHERE 
+                    `problemId` = @problemId 
+                ORDER BY 
+                    `id` ASC
+                ;
+            ";
+
+            // Отправляем запрос на сервер БД
+            var cmdSelect = new MySqlCommand(querySelect, connection);
+            cmdSelect.Parameters.AddWithValue("@problemId", submissionInfo.ProblemId);
+
+            // Получаем результат выполнения запроса
+            var dataReader = cmdSelect.ExecuteReader();
+
+            /*
+             * Для хранения и использования
+             * информации о релизных тестах
+             * пользовательского    решения
+             * поставленной  задачи создаём
+             * специальную очередь, которая
+             * будет хранить объекты содер-
+             * жащие  информацию о релизных
+             * тестах для заданной  задачи.
+             */
+            var testsQueue = new Queue<ReleaseTest>();
+
+            /*
+             * В цикле занимаемся обработкой
+             * каждого    релизного    теста
+             * пользовательских      решений
+             * и вносим  его  в  специальную
+             * очередь тестов.
+             */
+            while (dataReader.Read())
+            {
+
+                /*
+                 * Создаём новый объект, содержащий
+                 * информацию об одном из  релизных
+                 * тестов пользовательских решений.
+                 */
+                var testInfo = new ReleaseTest(
+
+                    // Уникальный идентификатор теста
+                    int.Parse(
+                        HttpUtility.HtmlDecode(dataReader["id"].ToString())
+                    ),
+
+                    // Входные данные
+                    (byte[])dataReader["input"],
+
+                    // Выходные данные
+                    (byte[])dataReader["output"],
+
+                    // Лимит используемой памяти
+                    long.Parse(
+                        HttpUtility.HtmlDecode(dataReader["memoryLimit"].ToString())
+                    ),
+
+                    // Лимит используемого процессорного времени
+                    int.Parse(
+                        HttpUtility.HtmlDecode(dataReader["timeLimit"].ToString())
+                    )
+
+                );
+                
+                // Добавляем текущий тест в конец очереди обработки
+                testsQueue.Enqueue(testInfo);
+
+            }
+
+            // Завершаем чтение потока
+            dataReader.Close();
+
+            /*
+             * Возвращаем специально
+             * созданную     очередь
+             * тестов  для  заданной
+             * задачи.
+             */
+            return testsQueue;
 
         }
 
