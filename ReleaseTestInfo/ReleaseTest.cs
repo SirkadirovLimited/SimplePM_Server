71cd2bc945a5a4daaab51b49554bb7a64246af9a
Kadirov Yurij <admin@sirkadirov.com>
2018-02-10 16:05:02 +0200

    Добавлен новый проект по созданию DLL для нужд сервера проверки решений

---
+++ ReleaseTestInfo/ReleaseTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReleaseTestInfo
+{
+    
+    public class ReleaseTest
+    {
+
+        public int Id { get; } //!< Уникальный идентификатор теста
+
+        public byte[] InputData { get; } //!< Входные данные для теста
+        public byte[] OutputData { get; } //!< Выходные данные для теста
+
+        public long MemoryLimit { get; } //!< Лимит по используемой памяти
+        public int ProcessorTimeLimit { get; } //!< Лимит по используемому процессорному времени
+
+        public ReleaseTest(int id, byte[] input, byte[] output, long memoryLimit, int timeLimit)
+        {
+
+            /*
+             * Инициализируем все необходимые переменные
+             */
+
+            Id = id;
+            InputData = input;
+            OutputData = output;
+            MemoryLimit = memoryLimit;
+            ProcessorTimeLimit = timeLimit;
+
+        }
+
+    }
+
+}
