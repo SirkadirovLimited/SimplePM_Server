df6503839dfcc39a8562afd785c027a97c41d9cf
Kadirov Yurij <admin@sirkadirov.com>
2018-02-24 22:01:29 +0200

    Продолжается реализация SERVER-1 SERVER-3 №3

--- SubmissionInfo/SubmissionInfo.cs
+++ SubmissionInfo/SubmissionInfo.cs
@@ -9,33 +9,76 @@
  * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
  */
 
+using System;
+
 namespace SubmissionInfo
 {
 
+    /*
+     * Класс,   реализации   которого   являются
+     * хранилищами информации о пользовательских
+     * запросах на тестирование  решений  данных
+     * задач по программированию.
+     */
     public class SubmissionInfo
     {
 
+        /*
+         * Базовая информация о запросе
+         */
         public int SubmissionId { get; set; }
         public int UserId { get; set; }
+
+        /*
+         * Информация о принадлежности
+         * пользовательской  попытки к
+         * указанному   уроку  и / или
+         * олимпиаде.
+         */
         public int ClassworkId { get; set; }
         public int OlympId { get; set; }
-
-        public string CodeLang { get; set; }
+        
+        /*
+         * Информация о параметрах тестирования
+         */
         public string TestType { get; set; }
         public string CustomTest { get; set; }
 
+        /*
+         * Информация  об  исходном коде  пользовательского
+         * решения поставленной задачи по программированию.
+         */
+        public string CodeLang { get; set; }
         public byte[] ProblemCode { get; set; }
 
+        /*
+         * Дополнительная   информация,   которая
+         * помогает обрабатывать пользовательский
+         * запрос на тестирование решения.
+         */
         public ProblemInfo ProblemInformation;
         
     }
 
+    /*
+     * Класс описывает поля и методы,
+     * которые относятся к данной задаче,
+     * по которой пришёл запрос на тестирование.
+     */
     public class ProblemInfo
     {
 
-        public int ProblemId { get; set; }
-        public int ProblemDifficulty { get; set; }
-        public bool AdaptProgramOutput { get; set; }
+        // Уникальный идентификатор задачи
+        public int ProblemId { get; set; } = 0;
+
+        // Сложность задачи
+        public int ProblemDifficulty { get; set; } = 0;
+
+        // Указание, не строгая ли проверка выхода
+        public bool AdaptProgramOutput { get; set; } = true;
+
+    }
+
 
     }
 
