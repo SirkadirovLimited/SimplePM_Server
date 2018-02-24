5ce539dd2430c058ce396b425eea75b20012f530
Kadirov Yurij <admin@sirkadirov.com>
2018-02-24 22:01:39 +0200

    Продолжается реализация SERVER-1 SERVER-3 №4

--- SubmissionInfo/SubmissionInfo.cs
+++ SubmissionInfo/SubmissionInfo.cs
@@ -79,6 +79,46 @@ namespace SubmissionInfo
 
     }
 
+    /*
+     * Класс описывает поля и методы, которые
+     * ответственны за хранение и обработку
+     * результата выполнения проверки пользовательского
+     * решения данной задачи по программированию на
+     * правильность и соответствие всем перечисленным
+     * требованиям.
+     */
+    public class SubmissionResult
+    {
+
+        /*
+         * Выходные потоки компилятора по данному решению
+         */
+        public string CompilerOutput { get; set; } = string.Empty;
+
+        /*
+         * Информация  о  тестах  и  тестировании
+         * пользовательского решения поставленной
+         * задачи по программированию.
+         */
+        public int TestsCount { get; set; } = 0;
+        public int PassedTestsCount { get; set; } = 0;
+        public int FailedTestsCount { get; set; } = 0;
+
+        /*
+         * Тип оценивания пользовательского решения
+         */
+        public RatingType SubmissionRatingType { get; set; } = RatingType.FULL;
+
+        /*
+         * Данный  enum  перечисляет  все  виды
+         * оценивания пользовательского решения
+         * поставленной задачи.
+         */
+        public enum RatingType
+        {
+            TEST_BY_TEST = 0,
+            FULL = 1
+        }
 
     }
 
