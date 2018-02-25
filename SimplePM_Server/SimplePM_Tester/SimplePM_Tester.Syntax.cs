810acae37b92a0936a50fb4071b6aad460a1de14
Kadirov Yurij <admin@sirkadirov.com>
2018-02-25 16:15:04 +0200

    Реализованы методы проверки синтаксиса SERVER-1 SERVER-3

---
+++ SimplePM_Server/SimplePM_Tester/SimplePM_Tester.Syntax.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Copyright (C) 2017, Kadirov Yurij.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ * 
+ * @Author: Kadirov Yurij
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
+ */
+
+using CompilerBase;
+
+namespace SimplePM_Server.SimplePM_Tester
+{
+    
+    internal partial class SimplePM_Tester
+    {
+
+        public ProgramTestingResult Syntax(CompilerResult compilingResult)
+        {
+
+            /*
+             * Генерируем результат тестирования
+             * и возвращаем его  как объект типа
+             * ProgramTestingResult.
+             */
+            return new ProgramTestingResult(1)
+            {
+
+                /*
+                 * Указываем информацию об единственном тесте,
+                 * который сигнализирует об успешности выполнения
+                 * процесса компиляции пользовательского решения.
+                 */
+                
+                TestingResults =
+                {
+                    
+                    [0] = new Test
+                    {
+                        
+                        Output = compilingResult.CompilerMessage,
+                        Result = (!compilingResult.HasErrors) ? '+' : '-',
+                        UsedMemory = 0,
+                        UsedProcessorTime = 0,
+                        ExitCode = 0
+
+                    }
+
+                }
+
+            };
+
+
+        }
+
+    }
+
+}
