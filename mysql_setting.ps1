6114abb26a34495b4164783b9e626f8b4f9716f5
Kadirov Yurij <admin@sirkadirov.com>
2018-02-25 10:41:53 +0200

    Начало реализации SERVER-12 - скачивание дампа БД

---
+++ mysql_setting.ps1
@@ -0,0 +1,14 @@
+﻿# Объявление и инициализация некоторых переменных
+$url = "https://raw.githubusercontent.com/SirkadirovTeam/SimplePM_WebClient/master/inc/simplepm.sql"
+$output = "$PSScriptRoot\simplepm.sql"
+$start_time = Get-Date
+
+Write-Output "Downloading SimplePM database dump file from $($url) to $($output)"
+
+# Запуск скрипта загрузки файла
+Invoke-WebRequest -Uri $url -OutFile $output
+
+Write-Output "File downloading completed (or not)"
+
+# Оповещаем пользователя о прошедшем времени
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)."
