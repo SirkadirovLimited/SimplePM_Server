a8e3bb5b9a913c6bb34651f8827fe6a24449283c
Kadirov Yurij <admin@sirkadirov.com>
2018-03-11 11:13:01 +0200

    Начало создания утилиты для автоматического тестирования сервера проверки решений SERVER-12 что в планах SERVER-3

---
+++ ServerTester/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CommandDotNet;
+using CommandDotNet.Models;
+
+namespace ServerTester
+{
+
+    public class Program
+    {
+
+        public static int Main(string[] args)
+        {
+
+            AppSettings settings = new AppSettings()
+            {
+                AllowArgumentSeparator = true,
+                Case = Case.LowerCase,
+                EnableVersionOption = true,
+                ShowArgumentDetails = true,
+                ThrowOnUnexpectedArgument = true
+            };
+
+            AppRunner<Tester> runner = new AppRunner<Tester>(settings);
+
+            return runner.Run(args);
+
+        }
+
+    }
+
+}
---
+++ ServerTester/Tester.cs
@@ -0,0 +1,94 @@
+﻿using CommandDotNet.Attributes;
+
+namespace ServerTester
+{
+    class Tester
+    {
+
+        [ApplicationMetadata(
+            Description = "Get submissions list",
+            Name = "submissions"
+        )]
+        public void GetSubmissionsList(
+            [Option(
+                Description = "Class work identificatior"
+            )] int classworkId = 0,
+            [Option(
+                Description = "Programming competition identificatior"
+            )] int olympId = 0,
+            [Option(
+                Description = "Status of the submission (waiting, processing, ready)"
+            )] string status = "ready"
+        )
+        {
+
+
+
+        }
+
+        [ApplicationMetadata(
+            Description = "Send submission to a server",
+            Name = "sendsubm"
+        )]
+        public void SendSubmission(
+            [Argument(
+                Name = "problemid",
+                Description = "Problem identificatior"
+            )] int problemId,
+
+            [Argument(
+                Name = "codepath",
+                Description = "Submission source code path"
+            )] string codePath,
+            [Argument(
+                Name = "codelang",
+                Description = "Submission source code language name"
+            )] string codeLang,
+
+            [Argument(
+                Name = "type",
+                Description = "Submission type (syntax, debug, release)"
+            )] string type,
+
+            [Option(
+                Description = "User identificator"
+            )] int userId = 0,
+            [Option(
+                Description = "Class work identificator"
+            )] int classworkId = 0,
+            [Option(
+                Description = "Programming competition identificator"
+            )] int olympId = 0
+        )
+        {
+
+
+
+        }
+
+        [ApplicationMetadata(
+            Description = "Active wait a period of time to the completion of submissions checking",
+            Name = "waitsubm"
+        )]
+        public void WaitComplete(
+            [Option(
+                Description = "Class work identificatior"
+            )] int classworkId = 0,
+            [Option(
+                Description = "Programming competition identificatior"
+            )] int olympId = 0,
+            [Option(
+                Description = "Status of the submission (waiting, processing, ready)"
+            )] string status = "ready",
+            [Option(
+                Description = "Check timeout (in ms)"
+            )] int timeout = 5000
+        )
+        {
+
+
+
+        }
+
+    }
+}
