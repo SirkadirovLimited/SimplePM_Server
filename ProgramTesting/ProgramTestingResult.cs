7083e79d4f08682490415725b67700fe61c4c723
Kadirov Yurij <admin@sirkadirov.com>
2018-03-18 18:20:39 +0200

    Некоторые типы данных перенесены в отдельную библиотеку SERVER-3

---
+++ ProgramTesting/ProgramTestingResult.cs
@@ -0,0 +1,344 @@
+﻿/*
+ * Copyright (C) 2017, Kadirov Yurij.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ * 
+ * @Author: Kadirov Yurij
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
+ */
+
+using System;
+using System.Linq;
+
+namespace ProgramTesting
+{
+
+    /*
+     * Класс, который является хостом
+     * для  результатов  тестирования
+     * пользовательского      решения
+     * поставленной     задачи     по
+     * программированию.
+     */
+
+    public class ProgramTestingResult
+    {
+
+        /*
+         * Массив,   содержащий  информацию
+         * о тестировании пользовательского
+         * решения.
+         */
+
+        public TestResult[] TestingResults;
+
+        /*
+         * Поле класса, которое возвращает
+         * общее количество тестов.
+         */
+
+        public int TestsCount => TestingResults.Length;
+
+        /*
+         * Метод определяет количество
+         * успешно пройденных тестов.
+         */
+
+        public int PassedTestsCount()
+        {
+
+            /*
+             * Вычисляем сумму IsSuccessful значений
+             * всех элементов массива TestingResults
+             * и возвращаем полученное значение.
+             */
+
+            return TestingResults.Sum(test => Convert.ToInt32(test.IsSuccessful));
+
+        }
+
+        /*
+         * Метод  является   переопределением
+         * стандартного конструктора текущего
+         * класса.
+         */
+
+        public ProgramTestingResult(int testsCount)
+        {
+
+            // Инициализируем массив результатов тестирования
+            TestingResults = new TestResult[testsCount];
+
+        }
+
+        /*
+         * Метод возвращает строку, которая содержит
+         * информацию  о  результатах   тестирования
+         * пользовательской программы.
+         */
+
+        public string GetResultAsLine(char splitter)
+        {
+
+            /*
+             * Объявляем временную переменную, в которой
+             * будем хранить результат работы метода
+             */
+
+            string resultLine;
+
+            try
+            {
+
+                /*
+                 * С помощью LINQ зароса формируем
+                 * строку результата.
+                 */
+
+                resultLine = TestingResults.Aggregate(
+                    "",
+                    (current, test) => current + (test.Result.ToString() + splitter.ToString())
+                );
+
+                /*
+                 * Удаляем все ненужные символы
+                 */
+                resultLine = resultLine.Trim('\n', '\r');
+
+            }
+            catch (Exception)
+            {
+
+                // Если что-то сломалось, возвращаем пустоту
+                resultLine = "";
+
+            }
+
+            // Возвращаем сформированную строку
+            return resultLine;
+
+        }
+
+        /*
+         * Метод возвращает строку,  которая  содержит
+         * информацию о кодах выхода пользовательского
+         * процесса на всех тестах.
+         */
+
+        public string GetExitCodesAsLine(char splitter)
+        {
+
+            /*
+             * Объявляем временную переменную, в которой
+             * будем хранить результат работы метода
+             */
+
+            string resultLine;
+
+            try
+            {
+
+                /*
+                 * С помощью LINQ зароса формируем
+                 * строку результата.
+                 */
+
+                resultLine = TestingResults.Aggregate(
+                    "",
+                    (current, test) => current + (test.ExitCode.ToString() + splitter.ToString())
+                );
+
+                /*
+                 * Удаляем все ненужные символы
+                 */
+
+                resultLine = resultLine.Trim('\n', '\r');
+
+            }
+            catch (Exception)
+            {
+
+                // Если что-то сломалось, возвращаем пустоту
+                resultLine = "";
+
+            }
+
+            // Возвращаем сформированную строку
+            return resultLine;
+
+        }
+
+        /*
+         * Метод возвращает строку, которая содержит
+         * информацию  об  использованной  процессом
+         * памяти во время каждого теста.
+         */
+
+        public string GetUsedMemoryAsLine(char splitter)
+        {
+
+            /*
+             * Объявляем временную переменную, в которой
+             * будем хранить результат работы метода
+             */
+
+            string resultLine;
+
+            try
+            {
+
+                /*
+                 * С помощью LINQ зароса формируем
+                 * строку результата.
+                 */
+
+                resultLine = TestingResults.Aggregate(
+                    "",
+                    (current, test) => current + (test.UsedMemory.ToString() + splitter.ToString())
+                );
+
+                /*
+                 * Удаляем все ненужные символы
+                 */
+
+                resultLine = resultLine.Trim('\n', '\r');
+
+            }
+            catch (Exception)
+            {
+
+                // Если что-то сломалось, возвращаем пустоту
+                resultLine = "";
+
+            }
+
+            // Возвращаем сформированную строку
+            return resultLine;
+
+        }
+
+        /*
+         * Метод возвращает строку, которая содержит
+         * информацию  об  использованной  процессом
+         * процессорного  времени  во  время каждого
+         * теста.
+         */
+
+        public string GetUsedProcessorTimeAsLine(char splitter)
+        {
+
+            /*
+             * Объявляем временную переменную, в которой
+             * будем хранить результат работы метода
+             */
+
+            string resultLine;
+
+            try
+            {
+
+                /*
+                 * С помощью LINQ зароса формируем
+                 * строку результата.
+                 */
+
+                resultLine = TestingResults.Aggregate(
+                    "",
+                    (current, test) => current + (test.UsedProcessorTime.ToString() + splitter.ToString())
+                );
+
+                /*
+                 * Удаляем все ненужные символы
+                 */
+
+                resultLine = resultLine.Trim(splitter);
+
+            }
+            catch (Exception)
+            {
+
+                // Если что-то сломалось, возвращаем пустоту
+                resultLine = "";
+
+            }
+
+            // Возвращаем сформированную строку
+            return resultLine;
+
+        }
+
+        /*
+         * Метод возвращает строку исключений
+         * тестируемой   программы,   которая
+         * собрана со всех тестов.
+         */
+
+        public string GetErrorOutputAsLine()
+        {
+
+            /*
+             * Объявляем временную переменную, в которой
+             * будем хранить результат работы метода
+             */
+
+            string resultLine;
+
+            try
+            {
+
+                /*
+                 * С помощью LINQ зароса формируем
+                 * строку результата.
+                 */
+
+                resultLine = TestingResults.Aggregate(
+                    "",
+                    (current, test) => current + (test.ErrorOutput + "\r\n")
+                );
+
+                /*
+                 * Удаляем все ненужные символы
+                 */
+
+                resultLine = resultLine.Trim('\n', '\r');
+
+            }
+            catch (Exception)
+            {
+
+                // Если что-то сломалось, возвращаем пустоту
+                resultLine = "";
+
+            }
+
+            // Возвращаем сформированную строку
+            return resultLine;
+
+        }
+
+        /*
+         * Метод возвращает булевое значение,   которое
+         * гласит о том, успешно ли пройдены все тесты,
+         * или нет. Ошибок, вроде, не должно возникать.
+         */
+
+        public bool IsSuccessful()
+        {
+
+            /*
+             * Выполняем LINQ запрос на выборку и проверку, после
+             * чего возвращаем результат выполнения метода.
+             */
+
+            return TestingResults.Aggregate(
+                true,
+                (current, test) => current && test.IsSuccessful
+            );
+
+        }
+
+    }
+
+}
---
+++ ProgramTesting/TestResult.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Copyright (C) 2017, Kadirov Yurij.
+ * All rights are reserved.
+ * Licensed under Apache License 2.0 with additional restrictions.
+ * 
+ * @Author: Kadirov Yurij
+ * @Website: https://sirkadirov.com/
+ * @Email: admin@sirkadirov.com
+ * @Repo: https://github.com/SirkadirovTeam/SimplePM_Server
+ */
+
+namespace ProgramTesting
+{
+
+    /*
+     * Класс, который является примитивом
+     * единичного     теста     заданного
+     * пользовательского  решения  данной
+     * задачи по программированию.
+     */
+
+    public class TestResult
+    {
+
+        /*
+         * Раздел объявления важных констант
+         */
+
+        public const char MiddleSuccessResult = '*'; // Предварительный успешный результат
+        public const char FullSuccessResult = '+'; // Успешный результат
+        public const char FullNoSuccessResult = '-'; // Не успешный результат
+        public const char TimeLimitResult = 'T'; // Превышен лимит по использованному процессорному времени
+        public const char MemoryLimitResult = 'M'; // Превышен лимит по использованной памяти
+        public const char RuntimeErrorResult = 'R'; // При выполнении произошла Runtime-ошибка
+        public const char ErrorOutputNotNullResult = 'E'; // Поток ошибок не пустой
+        public const char InputErrorResult = 'I'; // Ошибка записи входного потока
+        public const char OutputErrorResult = 'O'; // Ошибка в формате выходного потока
+        public const char ServerErrorResult = 'C'; // Ошибка сервера проверки решений
+
+        /*
+         * Раздел объявления переменных
+         */
+
+        public string ErrorOutput { get; set; } // Выходной поток ошибок пользовательской программы
+        public byte[] Output { get; set; } // Выходной поток пользовательской программы
+
+        public int ExitCode { get; set; } // Код выхода пользовательской программы
+        public char Result { get; set; } // Результат по текущему тесту
+
+        public int UsedProcessorTime { get; set; } // Использование процессорного времени пользовательской программой
+        public long UsedMemory { get; set; } // Использование памяти пользовательской программой
+
+        public bool IsSuccessful => (
+            Result == '+' && ExitCode == 0
+        ); // Указывает на то, пройден ли тест или нет
+
+    }
+
+}
