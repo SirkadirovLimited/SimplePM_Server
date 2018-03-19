8fbf0564a406acfc7cca9d8ebeb6e02fe7031685
Kadirov Yurij <admin@sirkadirov.com>
2018-03-19 22:33:57 +0200

    К коду предоставлены подробные комментарии

--- JudgeBase/IJudgePlugin.cs
+++ JudgeBase/IJudgePlugin.cs
@@ -14,12 +14,38 @@ using ProgramTesting;
 namespace JudgeBase
 {
 
+    /*
+     * Интерфейс описывает структуру
+     * типичного плагина-судьи.
+     */
+
     public interface IJudgePlugin
     {
 
-        string JudgeType { get; }
+        /*
+         * Наименование плагина-судьи
+         */
+
+        string JudgeName { get; }
+
+        /*
+         * Информация об авторе плагина-судьи
+         */
+
         string JudgeAuthor { get; }
+
+        /*
+         * Url-адрес технической поддержки
+         * для текущего плагина-судьи.
+         */
+
         string JudgeSupportUrl { get; }
+
+        /*
+         * Метод, который несёт ответственность
+         * за  вынесение  вердикта  о  рейтинге
+         * данного решения поставленной задачи.
+         */
 
         JudgeResult GenerateJudgeResult(ref ProgramTestingResult programTestingResult);
 
