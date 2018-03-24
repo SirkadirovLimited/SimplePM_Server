22d7fe6ef52604798134b7d35b07dafb2e16c88c
Kadirov Yurij <admin@sirkadirov.com>
2018-03-24 19:15:20 +0200

    Удалены упоминания Докера, восстановлены проекты + исключение некоторых проектов из дефолтной сборки

---
+++ ServerTester/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using CommandDotNet;
+using CommandDotNet.Models;
+
+namespace ServerTester
+{
+
+    public class Program
+    {
+
+        public const string configFilePath = "../config/database.json";
+
+        public static int Main(string[] args)
+        {
+
+            // Проверяем на существование конфигурационного файла
+            CheckConfigFileExists();
+
+            // Формируем настройки парсера аргументов
+            AppSettings settings = new AppSettings()
+            {
+                AllowArgumentSeparator = true,
+                Case = Case.LowerCase,
+                EnableVersionOption = true,
+                ShowArgumentDetails = true,
+                ThrowOnUnexpectedArgument = true
+            };
+
+            // Создаём экземпляр класса парсера аргументов
+            AppRunner<Tester> runner = new AppRunner<Tester>(settings);
+
+            // Запускаем парсер
+            return runner.Run(args);
+
+        }
+
+        private static void CheckConfigFileExists()
+        {
+
+            // Проверка на существование конфигурационного файла
+            if (!File.Exists(configFilePath))
+                throw new FileNotFoundException("Database config file is not found!", configFilePath);
+
+        }
+
+    }
+
+}
---
+++ ServerTester/Tester.cs
@@ -0,0 +1,114 @@
+﻿using CommandDotNet.Attributes;
+
+namespace ServerTester
+{
+
+    public class Tester
+    {
+
+        /*
+         * Получение списка запросов на тестирование
+         */
+
+        [ApplicationMetadata(
+            Description = "Get submissions list",
+            Name = "submissions"
+        )]
+        public void GetSubmissionsList(
+            [Option(
+                Description = "Class work identificatior"
+            )] long classworkId = 0,
+            [Option(
+                Description = "Programming competition identificatior"
+            )] long olympId = 0,
+            [Option(
+                Description = "Status of the submission (waiting, processing, ready)"
+            )] string status = "ready",
+            [Option(
+                Description = "User identificator"
+            )] long userId = -1
+        )
+        {
+
+
+
+        }
+
+        /*
+         * Отправка запроса(ов) на тестирование
+         */
+
+        [ApplicationMetadata(
+            Description = "Send submission to a server",
+            Name = "sendsubm"
+        )]
+        public void SendSubmission(
+            [Argument(
+                Name = "problemid",
+                Description = "Problem identificatior"
+            )] long problemId,
+
+            [Argument(
+                Name = "codepath",
+                Description = "Submission source code path"
+            )] string codePath,
+            [Argument(
+                Name = "codelang",
+                Description = "Submission source code language name"
+            )] string codeLang,
+
+            [Argument(
+                Name = "type",
+                Description = "Submission type (syntax, debug, release)"
+            )] string type,
+
+            [Option(
+                Description = "User identificator"
+            )] long userId = 0,
+            [Option(
+                Description = "Class work identificator"
+            )] long classworkId = 0,
+            [Option(
+                Description = "Programming competition identificator"
+            )] long olympId = 0,
+            [Option(
+                Description = "Programming competition identificator"
+            )] long submCount = 1
+        )
+        {
+
+
+
+        }
+
+        /*
+         * Ожидание завершения тестирования
+         */
+
+        [ApplicationMetadata(
+            Description = "Active wait a period of time to the completion of submissions checking",
+            Name = "waitsubm"
+        )]
+        public void WaitComplete(
+            [Option(
+                Description = "Class work identificatior"
+            )] long classworkId = 0,
+            [Option(
+                Description = "Programming competition identificatior"
+            )] long olympId = 0,
+            [Option(
+                Description = "Status of the submission (waiting, processing, ready)"
+            )] string status = "ready",
+            [Option(
+                Description = "Check timeout (in ms)"
+            )] long timeout = 5000
+        )
+        {
+
+
+
+        }
+
+    }
+
+}
