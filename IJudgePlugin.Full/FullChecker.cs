9b72ab9aeb99ce84b07aa6fa1750efe3c8b4ba8a
Kadirov Yurij <admin@sirkadirov.com>
2018-04-13 15:18:26 +0300

    Продолжается реализация новой архитектуры сервера проверки решений SERVER-3

---
+++ IJudgePlugin.Full/FullChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JudgeBase;
+using ProgramTesting;
+
+namespace JudgePlugin
+{
+    public class Judge : IJudgePlugin
+    {
+
+        public string JudgeName => "full";
+
+        public string JudgeAuthor => "Yurij Kadirov";
+
+        public string JudgeSupportUrl => "https://spm.sirkadirov.com/";
+
+        public JudgeResult GenerateJudgeResult(ref ProgramTestingResult programTestingResult)
+        {
+
+            return new JudgeResult
+            {
+                RatingMult = Convert.ToInt32(programTestingResult.PassedTestsCount() >= programTestingResult.TestsCount)
+            };
+
+        }
+
+    }
+}
