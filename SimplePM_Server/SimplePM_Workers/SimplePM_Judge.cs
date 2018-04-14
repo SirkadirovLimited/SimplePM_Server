4d65b76186a1f8df44dd991c32e1e52f0850a95c
Kadirov Yurij <admin@sirkadirov.com>
2018-04-14 15:10:43 +0300

    Продолжается реализация модулей оценивания SERVER-3

---
+++ SimplePM_Server/SimplePM_Workers/SimplePM_Judge.cs
@@ -0,0 +1,43 @@
+﻿using JudgeBase;
+
+namespace SimplePM_Server
+{
+
+    internal class SimplePM_Judge
+    {
+
+        /*
+         * Метод осуществляет поиск
+         * модуля оценивания пользо
+         * вательского решения пост
+         * авленной задачи  по  его
+         * названию.
+         */
+
+        public IJudgePlugin GetJudgePluginByName(string judgeName)
+        {
+
+            /*
+             * В цикле производим поиск
+             * по списку плагинов оцени
+             * вания  пользовательского
+             * решения.
+             */
+
+            foreach (IJudgePlugin judgePlugin in SimplePM_Worker._judgePlugins)
+            {
+
+                // Если мы нашли то, что искали, то это хорошо
+                if (judgePlugin.JudgeName == judgeName)
+                    return judgePlugin;
+
+            }
+
+            // Всё плохо, мы ничего не нашли!
+            return null;
+
+        }
+
+    }
+
+}
