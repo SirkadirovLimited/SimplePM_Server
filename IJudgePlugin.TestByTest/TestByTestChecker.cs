6bd6b0c0d3902580a04f8718313da77f6322e26a
Kadirov Yurij <admin@sirkadirov.com>
2018-04-29 15:31:46 +0300

    Реализован плагин потестового оценивания решения пользовательской программы SERVER-3

---
+++ IJudgePlugin.TestByTest/TestByTestChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JudgeBase;
+using ProgramTesting;
+
+namespace JudgePlugin
+{
+    public class Judge : IJudgePlugin
+    {
+
+        public string JudgeName => "testbytest";
+
+        public string JudgeAuthor => "Yurij Kadirov";
+
+        public string JudgeSupportUrl => "https://spm.sirkadirov.com/";
+
+        public JudgeResult GenerateJudgeResult(ref ProgramTestingResult programTestingResult)
+        {
+
+            return new JudgeResult
+            {
+                RatingMult = programTestingResult.PassedTestsCount() / (float)programTestingResult.TestsCount
+            };
+
+        }
+
+    }
+}
