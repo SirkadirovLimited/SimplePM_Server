a252856d67dfa4594212611a82210ca961040d7c
Kadirov Yurij <admin@sirkadirov.com>
2018-05-08 17:19:17 +0300

    Добавлен новый тип судьи по-умолчанию SERVER-3

---
+++ IJudgePlugin.None/NoChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JudgeBase;
+using ProgramTesting;
+
+namespace JudgePlugin
+{
+    public class Judge : IJudgePlugin
+    {
+
+        public string JudgeName => "none";
+
+        public string JudgeAuthor => "Yurij Kadirov";
+
+        public string JudgeSupportUrl => "https://spm.sirkadirov.com/";
+
+        public JudgeResult GenerateJudgeResult(ref ProgramTestingResult programTestingResult)
+        {
+
+            return new JudgeResult
+            {
+                RatingMult = 0
+            };
+
+        }
+
+    }
+}
