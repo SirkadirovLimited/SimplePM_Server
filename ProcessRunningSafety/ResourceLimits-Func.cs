a4ca46e13cbd51c972cb0d96b36d58a2a680b2af
Kadirov Yurij <admin@sirkadirov.com>
2018-05-15 11:47:29 +0300

    Начало создания раздельной подсистемы для ограничения пользовательских ресурсов SERVER-5 SERVER-22

---
+++ ProcessRunningSafety/ResourceLimits-Func.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessRunningSafety
+{
+
+    public partial class ResourceLimits
+    {
+
+        public int SetWindowsLimits(int cpuTimeLimit, long memLimit)
+        {
+            
+
+
+        }
+
+    }
+
+}
---
+++ ProcessRunningSafety/ResourceLimits.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessRunningSafety
+{
+
+    /// <summary>
+    /// Данный класс предоставляет функционал для
+    /// ограничения ресурсов указанного процесса,
+    /// в зависимости от используемой операционной
+    /// системы выполняя соответствующие системные
+    /// вызовы и прочие полезные штуки.
+    /// </summary>
+
+    public partial class ResourceLimits
+    {
+
+        /// <summary>
+        /// Идентификатор связанного процесса
+        /// </summary>
+
+        public int ProcessId { get; }
+
+        /// <summary>
+        /// Основной конструктор класса ResourceLimits
+        /// </summary>
+        /// <param name="processId">Идентификатор связанного процесса</param>
+        
+        public ResourceLimits(int processId)
+        {
+
+            /*
+             * Проверка на существование процесса
+             * с указанным идентификатором.
+             */
+
+            try
+            {
+
+                // Если всё плохо - выбросит исключение
+                Process.GetProcessById(processId);
+
+            }
+            catch (Exception ex)
+            {
+
+                // Идентификатор не верен, выбрасываем исключение
+                throw new ArgumentException(
+                    "Process associated with ID " + processId + " not found!",
+                    nameof(processId),
+                    ex
+                );
+
+            }
+
+            // Сохраняем полученный идентификатор
+            ProcessId = processId;
+
+        }
+
+    }
+
+}
