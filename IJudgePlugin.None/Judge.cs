e4544bb4ff376c71d4c1b59b399e22e5cb9b554b
Kadirov Yurij <admin@sirkadirov.com>
2018-07-03 16:19:52 +0300

    Реализован IJudgePlugin.None SERVER-35

---
+++ IJudgePlugin.None/Judge.cs
@@ -0,0 +1,28 @@
+﻿using Plugin;
+using ProgramTestingAdditions;
+
+namespace JudgePlugin
+{
+    
+    public class Judge : IJudgePlugin
+    {
+        
+        public PluginInfo PluginInformation => new PluginInfo(
+            "none",
+            "Yurij Kadirov (Sirkadirov)",
+            "https://spm.sirkadirov.com/"
+        );
+
+        public JudgingResult GenerateJudgeResult(ref ProgramTestingResult programTestingResult)
+        {
+
+            return new JudgingResult
+            {
+                RatingMult = 0
+            };
+
+        }
+
+    }
+    
+}
