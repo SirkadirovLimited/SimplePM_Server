ba1b15b46dc379c05a9ecff8bb2fe865b5030037
Yurij Kadirov <admin@sirkadirov.com>
2018-07-16 12:45:01 +0300

    Виконання SERVER-44

---
+++ ICompilerPlugin.DotNetCore/DotNetCore.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * ███████╗██╗███╗   ███╗██████╗ ██╗     ███████╗██████╗ ███╗   ███╗
+ * ██╔════╝██║████╗ ████║██╔══██╗██║     ██╔════╝██╔══██╗████╗ ████║
+ * ███████╗██║██╔████╔██║██████╔╝██║     █████╗  ██████╔╝██╔████╔██║
+ * ╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  ██╔═══╝ ██║╚██╔╝██║
+ * ███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗██║     ██║ ╚═╝ ██║
+ * ╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝     ╚═╝     ╚═╝
+ *
+ * SimplePM Server is a part of software product "Automated
+ * verification system for programming tasks "SimplePM".
+ *
+ * Copyright 2018 Yurij Kadirov
+ *
+ * Source code of the product licensed under the Apache License,
+ * Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Visit website for more details: https://spm.sirkadirov.com/
+ */
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Plugin;
+
+namespace CompilerPlugin
+{
+    
+    // ReSharper disable once UnusedMember.Global
+    public class Compiler : ICompilerPlugin
+    {
+
+        public PluginInfo PluginInformation => new PluginInfo(
+            "DotNetCore",
+            "Yurij Kadirov (Sirkadirov)",
+            "https://spm.sirkadirov.com/"
+        );
+
+        public CompilationResult RunCompiler(ref dynamic languageConfiguration, string submissionId, string fileLocation)
+        {
+
+            // Получаем путь к директории с исходным кодом
+            var directoryFullPath = new FileInfo(fileLocation).DirectoryName ?? throw new FileNotFoundException();
+            
+            // Создаём новый .NET Core проект
+            var generationResult = StandardCompilationMethods.RunCompiler(
+                "dotnet",
+                "new console --name " + submissionId + " --language c# --output " + directoryFullPath
+            );
+
+            // Удаляем стандартный файл исходного кода
+            File.Delete(Path.Combine(directoryFullPath, "Program.cs"));
+            
+            // Выполняем сборку проекта средствами .NET Core's MSBuild
+            var buildResult = StandardCompilationMethods.RunCompiler(
+                "dotnet",
+                "build " + Path.Combine(directoryFullPath, submissionId + ".csproj") + " --language c# --output " + directoryFullPath
+            );
+
+            // Формируем полные выходные данные приложения
+            buildResult.CompilerOutput = generationResult.CompilerOutput + "\r\n" + buildResult.CompilerOutput;
+            
+            // Формируем полный путь к исполнительному файлу
+            buildResult.ExeFullname = Path.Combine(directoryFullPath, submissionId + ".dll");
+            
+            // Возвращаем результат компиляции
+            return StandardCompilationMethods.ReturnCompilerResult(buildResult);
+
+        }
+        
+        public bool SetRunningMethod(ref dynamic languageConfiguration, ref ProcessStartInfo startInfo, string filePath)
+        {
+            
+            try
+            {
+                
+                // Устанавливаем имя запускаемой программы
+                startInfo.FileName = (string)(languageConfiguration.runtime_path);
+                
+                // Аргументы запуска данной программы
+                startInfo.Arguments = "-d64 -cp . \"" + Path.GetFileNameWithoutExtension(new FileInfo(filePath).Name) + '"';
+
+            }
+            catch
+            {
+
+                // Сигнализируем о наличии ошибок в процессе операции
+                return false;
+
+            }
+            
+            // Сигнализируем об успешном выполнении операции
+            return true;
+
+        }
+        
+    }
+
+}
