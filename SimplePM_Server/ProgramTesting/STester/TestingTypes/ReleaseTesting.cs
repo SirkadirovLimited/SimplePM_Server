4f0dae37422b5d927749f62ba80a9eb969d4ba15
Yurij Kadirov <admin@sirkadirov.com>
2018-08-01 16:33:05 +0300

    Змінені заглавні коментарі до всіх файлів початкового коду GENERAL-1

--- SimplePM_Server/ProgramTesting/STester/TestingTypes/ReleaseTesting.cs
+++ SimplePM_Server/ProgramTesting/STester/TestingTypes/ReleaseTesting.cs
@@ -5,37 +5,38 @@
  * ╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  ██╔═══╝ ██║╚██╔╝██║
  * ███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗██║     ██║ ╚═╝ ██║
  * ╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝     ╚═╝     ╚═╝
- *
+ * 
  * SimplePM Server is a part of software product "Automated
  * verification system for programming tasks "SimplePM".
- *
- * Copyright 2018 Yurij Kadirov
- *
- * Source code of the product licensed under the Apache License,
- * Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * Copyright (C) 2016-2018 Yurij Kadirov
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * GNU Affero General Public License applied only to source code of
+ * this program. More licensing information hosted on project's website.
+ * 
  * Visit website for more details: https://spm.sirkadirov.com/
  */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
 using NLog;
+using System.Text;
 using MySql.Data.MySqlClient;
-
 using ProgramTestingAdditions;
-using SimplePM_Server.ProgramTesting.SRunner;
+using System.Collections.Generic;
 using SimplePM_Server.Workers.Recourse;
+using SimplePM_Server.ProgramTesting.SRunner;
 
 namespace SimplePM_Server.ProgramTesting.STester
 {
