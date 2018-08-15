eaa84424524824b650935cc3cf0f4338c4e71750
Yurij Kadirov <admin@sirkadirov.com>
2018-08-15 23:08:26 +0300

    Implementation of ProcessResourcesUsageStatsStruct struct, that stores process runtime statistics

---
+++ SProgramRunner/Additional/ProcessResourcesUsageStatsStruct.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * ███████╗██╗███╗   ███╗██████╗ ██╗     ███████╗██████╗ ███╗   ███╗
+ * ██╔════╝██║████╗ ████║██╔══██╗██║     ██╔════╝██╔══██╗████╗ ████║
+ * ███████╗██║██╔████╔██║██████╔╝██║     █████╗  ██████╔╝██╔████╔██║
+ * ╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  ██╔═══╝ ██║╚██╔╝██║
+ * ███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗██║     ██║ ╚═╝ ██║
+ * ╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝     ╚═╝     ╚═╝
+ * 
+ * SimplePM Server is a part of software product "Automated
+ * verification system for programming tasks "SimplePM".
+ * 
+ * Copyright (C) 2016-2018 Yurij Kadirov
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * GNU Affero General Public License applied only to source code of
+ * this program. More licensing information hosted on project's website.
+ * 
+ * Visit website for more details: https://spm.sirkadirov.com/
+ */
+
+using System;
+
+namespace SProgramRunner
+{
+    
+    /// <summary>
+    /// Stores resources usage statistics of specified process.
+    /// </summary>
+    public struct ProcessResourcesUsageStatsStruct
+    {
+
+        /// <summary>
+        /// Indicates total processor time, used by specified process.
+        /// </summary>
+        public int UsedProcessorTime { get; set; }
+
+        /// <summary>
+        /// Indicates peak working set, used by specified process.
+        /// </summary>
+        public long PeakUsedWorkingSet { get; set; }
+
+        /// <summary>
+        /// Indicates size of working directory of specified process.
+        /// </summary>
+        public long WorkingDirectoryDiskUsage { get; set; }
+
+        /// <summary>
+        /// Indicates real run time of specified process.
+        /// </summary>
+        public TimeSpan RealRunTime;
+
+    }
+    
+}
