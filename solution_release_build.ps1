fdee30ad54d3d7e31fc33f6824e06c21782f57a3
Yurij Kadirov <admin@sirkadirov.com>
2018-08-15 21:07:35 +0300

    Release build (publish) PowerShell script created

---
+++ solution_release_build.ps1
@@ -0,0 +1,20 @@
+Write-Output "|||||||||||||||||||||||||||||||||||||| BUILD STAGE ||||||||||||||||||||||||||||||||||||||"
+
+Write-Output "Restoring NuGet packages..."
+dotnet restore
+Write-Output "Restoring NuGet packages completed!"
+
+Write-Output "Removing temporary directories if they exist..."
+
+Remove-Item -path ./Build/ -recurse -whatif
+Remove-Item -path ./Build/ -recurse
+
+Write-Output "Temporary directories removed!"
+
+Write-Output "Publishing solution with .NET Core publish feature.."
+
+dotnet publish SimplePM_Server.sln --configuration Release --force --output ./Build/ --verbosity minimal
+
+Write-Output "Building (publishing) process finished!"
+
+Write-Output "||||||||||||||||||||||||||||||||||||||  STAGE END  ||||||||||||||||||||||||||||||||||||||"
