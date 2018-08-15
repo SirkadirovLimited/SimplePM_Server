6a63997796d89e837e7f50fb3a1bb72caf3849c6
Yurij Kadirov <admin@sirkadirov.com>
2018-08-15 23:06:31 +0300

    Base implementation of SRunner (maybe)

---
+++ SProgramRunner/SRunner/SRunner.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * ███████╗██╗███╗   ███╗██████╗ ██╗     ███████╗██████╗ ███╗   ███╗
+ * ██╔════╝██║████╗ ████║██╔══██╗██║     ██╔════╝██╔══██╗████╗ ████║
+ * ███████╗██║██╔████╔██║██████╔╝██║     █████╗  ██████╔╝██╔████╔██║
+ * ╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  ██╔═══╝ ██║╚██╔╝██║
+ * ███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗██║     ██║ ╚═╝ ██║
+ * ╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝     ╚═╝     ╚═╝
+ * 
+ * SimplePM Server is a part of software product "Automated
+ * verification system for programming tasks "SimplePM".
+ * 
+ * Copyright (C) 2016-2018 Yurij Kadirov
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * GNU Affero General Public License applied only to source code of
+ * this program. More licensing information hosted on project's website.
+ * 
+ * Visit website for more details: https://spm.sirkadirov.com/
+ */
+
+namespace SProgramRunner
+{
+    
+    public class SRunner
+    {
+
+        private readonly TestingRequestStuct _testingRequestStuct;
+        private ProcessResourcesUsageStatsStruct _processResourcesUsageStats;
+        
+        public SRunner(TestingRequestStuct testingRequestStuct)
+        {
+
+            // Store testing request data localy
+            _testingRequestStuct = testingRequestStuct;
+
+            // Reset process resources usage statistics
+            _processResourcesUsageStats = new ProcessResourcesUsageStatsStruct
+            {
+                
+                UsedProcessorTime = 0,
+                PeakUsedWorkingSet = 0,
+                WorkingDirectoryDiskUsage = 0
+                
+            };
+
+        }
+        
+    }
+    
+}
