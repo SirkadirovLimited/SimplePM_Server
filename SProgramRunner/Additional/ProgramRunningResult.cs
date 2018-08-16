02e84cf25cbdb9965236e978a549268341936180
Yurij Kadirov <admin@sirkadirov.com>
2018-08-16 17:02:46 +0300

    SProgramRunner.ProgramRunningResult realization

--- SProgramRunner/Additional/ProgramRunningResult.cs
+++ SProgramRunner/Additional/ProgramRunningResult.cs
@@ -42,8 +42,57 @@ namespace SProgramRunner
     public class ProgramRunningResult
     {
 
+        /// <summary>
+        /// Process runtime resources usage statistics.
+        /// </summary>
+        /// <seealso cref="ProcessResourcesUsageStatsStruct"/>
+        public ProcessResourcesUsageStatsStruct ProcessResourcesUsageStats { get; set; }
+
+        /// <summary>
+        /// Output data of specified process as bytes array
+        /// </summary>
+        public byte[] ProgramOutputData { get; set; }
+
+        /// <summary>
+        /// Data from process standard error output (<code>STDERR</code>).
+        /// </summary>
+        public string ProgramErrorData { get; set; }
+
+        /// <summary>
+        /// Exit code of the specified process.
+        /// </summary>
+        public int ProgramExitCode { get; set; }
+
+        /// <summary>
+        /// Result on current run.
+        /// </summary>
+        /// <see cref="TestingResult"/>
+        public char Result { get; set; }
         
+        /// <summary>
+        /// Check if current run is middle successful.
+        /// </summary>
+        /// <remarks>
+        /// Analog of manual check for <code>Result</code> equals to <code>TestingResult.MiddleSuccessResult</code>.
+        /// </remarks>
+        /// <see cref="TestingResult"/>
+        /// <seealso cref="TestingResult.MiddleSuccessResult"/>
+        public bool IsMiddleSuccessful => (
+            Result == TestingResult.MiddleSuccessResult
+        );
         
+        /// <summary>
+        /// Check if current run is fully succesful.
+        /// </summary>
+        /// <remarks>
+        /// Analog of manual check for <code>Result</code> equals to <code>TestingResult.FullSuccessResult</code>.
+        /// </remarks>
+        /// <see cref="TestingResult"/>
+        /// <seealso cref="TestingResult.FullSuccessResult"/>
+        public bool IsFullySuccessful => (
+            Result == TestingResult.FullSuccessResult
+        );
+
     }
     
 }
