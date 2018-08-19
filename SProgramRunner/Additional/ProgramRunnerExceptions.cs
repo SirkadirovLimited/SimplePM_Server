0f31e6163b571d8d49faa893463d7d9779126a83
Yurij Kadirov <admin@sirkadirov.com>
2018-08-19 17:51:45 +0300

    SProgramRunner: new static class ProgramRunnerExceptions with some new exceptions created

---
+++ SProgramRunner/Additional/ProgramRunnerExceptions.cs
@@ -0,0 +1,68 @@
+/*
+ * ███████╗██╗███╗   ███╗██████╗ ██╗     ███████╗██████╗ ███╗   ███╗
+ * ██╔════╝██║████╗ ████║██╔══██╗██║     ██╔════╝██╔══██╗████╗ ████║
+ * ███████╗██║██╔████╔██║██████╔╝██║     █████╗  ██████╔╝██╔████╔██║
+ * ╚════██║██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  ██╔═══╝ ██║╚██╔╝██║
+ * ███████║██║██║ ╚═╝ ██║██║     ███████╗███████╗██║     ██║ ╚═╝ ██║
+ * ╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝     ╚═╝     ╚═╝
+ * 
+ * SimplePM Server is a part of software product "Automated
+ * verification system for programming tasks "SimplePM".
+ * 
+ * Copyright (C) 2016-2018 Yurij Kadirov
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * GNU Affero General Public License applied only to source code of
+ * this program. More licensing information hosted on project's website.
+ * 
+ * Visit website for more details: https://spm.sirkadirov.com/
+ */
+
+using System;
+using System.Runtime.Serialization;
+
+namespace SProgramRunner
+{
+    
+    public static class ProgramRunnerExceptions
+    {
+
+        [Serializable]
+        public sealed class ProcessorUsageTimeLimitReachedException : Exception
+        {
+            
+            public ProcessorUsageTimeLimitReachedException() : base("Solution at run time reached used processor time limit!")
+            {
+                Source = "SProgramRunner";
+                HelpLink = "https://spm.sirkadirov.com/";
+            }
+
+        }
+        
+        [Serializable]
+        public sealed class MemoryUsageLimitReachedException : Exception
+        {
+            
+            public MemoryUsageLimitReachedException() : base("Solution at run time reached used memory limit!")
+            {
+                Source = "SProgramRunner";
+                HelpLink = "https://spm.sirkadirov.com/";
+            }
+
+        }
+        
+    }
+    
+}
